a3ec33547443ed37fd118aac4a07831299019690
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-05-14 23:41:08 +0530

    added router

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express from 'express'
 import userRouter from './routes/user'
 import messageRouter from './routes/message'
-
+import friendshipRouter from './routes/friendship'
 const app = express()
 app.use(express.json())
 
-app.use("/user",userRouter)
+app.use("/user",  userRouter)
 app.use("/message", messageRouter)
+app.use("/friend", friendshipRouter)
 
 app.get('/', (req,res)=>{
     res.send('express server is on')
