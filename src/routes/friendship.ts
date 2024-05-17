4cf1261dd49d7219f53239879a14b8139325054e
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-05-17 22:44:07 +0530

    added routes

--- src/routes/friendship.ts
+++ src/routes/friendship.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import { addFriend, getAllUserFriends } from '../controllers/friendship'
+import { authMiddleware } from '../middleware/auth'
 
 const router = express.Router()
 
-router.route('/').post(addFriend)
-router.route('/all').get(getAllUserFriends)
+router.route('/').post(authMiddleware,addFriend)
+router.route('/all').get(authMiddleware,getAllUserFriends)
 
 export default router
