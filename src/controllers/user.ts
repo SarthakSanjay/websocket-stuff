c6107485e29a708b1689d4ae207142384a0a4485
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-05-19 23:42:26 +0530

    added password for user auth

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,8 +1,10 @@
 import { PrismaClient } from "@prisma/client"
+import jwt from 'jsonwebtoken'
+import { CustomRequest } from "../middleware/auth"
 const prisma = new PrismaClient()
 
 export const registerUser = async(req,res)=>{
-    const {username , email} = req.body
+    const {username , email , password} = req.body
     
     const user = await prisma.user.findUnique({
         where:{
@@ -12,10 +14,19 @@ export const registerUser = async(req,res)=>{
     })
 
     if(!user){
+        const token = jwt.sign(
+            {
+                username: username,
+                email:email
+            },
+            process.env.TOKEN_SECRET
+        )
       const registeredUser =  await prisma.user.create({
             data:{
                 username:username,
-                email:email
+                email:email,
+                password:password,
+                token:token
             }
         })
 
@@ -31,7 +42,7 @@ export const registerUser = async(req,res)=>{
 }
 
 export const loginUser = async(req,res) =>{
-    const {username , email} = req.body
+    const {username , email ,password} = req.body
     
     const user = await prisma.user.findUnique({
         where:{
@@ -45,9 +56,29 @@ export const loginUser = async(req,res) =>{
             msg:"User not found"
         })
     }
-
+    if(!(user.password === password)){
+        return res.status(404).json({
+            msg:"password is incorrect"
+        })
+    }
     res.status(200).json({
         msg:"logged in succussfully",
         loggedIn : true
+    })
+}
+
+export const getAllUser = async(req :CustomRequest,res) =>{
+    const {id} = req.user
+    const user = await prisma.user.findUnique({
+        where:{id:id}
+    })
+    if(!user){
+        return res.status(401).json({msg:"not authorized"})
+    }
+
+    const allUser = await prisma.user.findMany()
+    res.status(200).json({
+        msg:"success",
+        allUser
     })
 }
