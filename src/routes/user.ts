72fe3748b0ece9d79608bf635337541d5fcf50f2
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-05-27 22:49:49 +0530

    added update user detail and search user routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/user";
+import { getAllUser, getUserDetails, loginUser, registerUser, searchUser, updateUserDetail } from "../controllers/user";
+import { authMiddleware } from "../middleware/auth";
 
 const router = express.Router()
 
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser)
-
+router.route('/all').get(authMiddleware , getAllUser )
+router.route('/details').get(authMiddleware,getUserDetails).patch(authMiddleware,updateUserDetail)
+router.route('/search').get(authMiddleware,searchUser)
 export default router
