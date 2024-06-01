40cd5fc6a0d417c9abb4179041ed2545cd806479
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-01 23:37:16 +0530

    added routes

--- src/routes/friendship.ts
+++ src/routes/friendship.ts
@@ -1,10 +1,11 @@
 import express from 'express'
-import { addFriend, getAllUserFriends } from '../controllers/friendship'
+import { acceptFriendReq, sendFriendRequest, getAllUserFriends, recivedFriendRequest } from '../controllers/friendship'
 import { authMiddleware } from '../middleware/auth'
 
 const router = express.Router()
 
-router.route('/').post(authMiddleware,addFriend)
+router.route('/request').post(authMiddleware,sendFriendRequest)
 router.route('/all').get(authMiddleware,getAllUserFriends)
-
+router.route('/accept').put(authMiddleware,acceptFriendReq)
+router.route('/all/request').get(authMiddleware,recivedFriendRequest)
 export default router
