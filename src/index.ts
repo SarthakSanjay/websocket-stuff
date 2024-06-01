1dcd5f719c654f9129c9085f0bcfdec61416ea95
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-01 23:37:41 +0530

    added cors

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import express from 'express'
 import userRouter from './routes/user'
 import messageRouter from './routes/message'
 import friendshipRouter from './routes/friendship'
+import cors from 'cors'
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.use("/user",  userRouter)
 app.use("/message", messageRouter)
