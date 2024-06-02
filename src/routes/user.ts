16a3024b1c6a9a9d08bf34879d329bb287545e5b
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-02 23:12:56 +0530

    added route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllUser, getUserDetails, loginUser, registerUser, searchUser, updateUserDetail } from "../controllers/user";
+import { getAllUser, getAdminDetails, loginUser, registerUser, searchUser, updateUserDetail, getUserDetails } from "../controllers/user";
 import { authMiddleware } from "../middleware/auth";
 
 const router = express.Router()
@@ -7,6 +7,7 @@ const router = express.Router()
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser)
 router.route('/all').get(authMiddleware , getAllUser )
-router.route('/details').get(authMiddleware,getUserDetails).patch(authMiddleware,updateUserDetail)
+router.route('/details').get(authMiddleware,getAdminDetails).patch(authMiddleware,updateUserDetail)
 router.route('/search').get(authMiddleware,searchUser)
+router.route('/:id').get(authMiddleware,getUserDetails)
 export default router
