c83491eab06280c6d32e435220c335354b659cae
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-05 23:27:24 +0530

    refactored websocket server

--- src/wsServer.ts
+++ src/wsServer.ts
@@ -12,29 +12,38 @@ interface CustomWebSocket extends WebSocket {
     userId : number
 }
 wss.on('connection', (connection : CustomWebSocket , req)=>{
-    let userId = url.parse(req.url, true).pathname.slice(1);
+    let userId = decodeURIComponent(url.parse(req.url, true).pathname.slice(1));
+    console.log(userId);
     connection.userId = parseInt(userId)
-    //@ts-ignore
+    // @ts-ignore
     connection.on('message', async(data)=>{
-        const {friendId , message} =  JSON.parse(data.toString())
+        const {friendId , text} =  JSON.parse(data.toString())
+
         const friends = await prisma.friendship.findFirst({
             where:{
-                //@ts-ignore
                 userId:parseInt(userId),
                 friendId:friendId
             }
         })
+        console.log(friendId , text);
         if(!friends){
-            console.log('not friends');
             connection.send('not friends')
             connection.close()
         }
-        wss.clients.forEach((client : any)=>{
+        
+        wss.clients.forEach(async(client : any)=>{
                 if(client.userId === friendId){
+                    await prisma.message.create({
+                        data:{
+                            senderId: parseInt(userId),
+                            receiverId: friendId,
+                            text: text
+                        }
+                    })
                     client.send(
                         JSON.stringify({
                             friendId: friendId,
-                            text : message,
+                            text : text,
                           })
                     )
                 }
