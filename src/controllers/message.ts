9fd3c8f89315bb400151badb11df08e753acd465
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-05 23:14:02 +0530

    removed dms and added getUserMessage

--- src/controllers/message.ts
+++ src/controllers/message.ts
@@ -1,31 +1,35 @@
 import { PrismaClient } from "@prisma/client"
+import { CustomRequest } from "../middleware/auth"
 const prisma = new PrismaClient()
 
-export const directMessage = async(req,res) =>{
-    // const senderId = req.user
-    const { receiverId ,senderId ,text} = req.body 
-
-    const receiver = await prisma.user.findUnique({
-        where:{
-            id:receiverId
-        }
-    })
-    if(!receiver){
-        return res.status(404).json({
-            msg:"User does not exists"
+export const getChatMessages = async(req:CustomRequest ,res)=>{
+    try {
+        const userId = req.user?.id
+        const friendId: number = parseInt(req.params.id)
+        // console.log('userID',userId,'friendID',friendId);
+        const messages = await prisma.message.findMany({
+            where:{
+                OR:[
+                    {senderId: userId , receiverId: friendId},
+                    {senderId: friendId , receiverId: userId},
+                ]
+            },select:{
+                id:true,
+                senderId: true,
+                receiverId: true,
+                text: true
+            }
+            ,
+            orderBy:{
+                createdAt:'asc'
+            }
+        })
+        res.status(200).json({
+            messages
+        })
+    } catch (error) {
+        res.status(404).json({
+            msg:'unexpected error'
         })
     }
-
-    const message = await prisma.message.create({
-        data:{
-            senderId:senderId,
-            receiverId:receiverId,
-            text:text
-        }
-    })
-
-    res.status(200).json({
-        msg:"DMed successfully",
-        message
-    })
 }
