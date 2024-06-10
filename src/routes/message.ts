99b06eb4f240fa18f2db18cac20c423fce764dec
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-10 22:38:04 +0530

    added delete message route

--- src/routes/message.ts
+++ src/routes/message.ts
@@ -1,7 +1,8 @@
 import express from 'express'
-import { getChatMessages } from '../controllers/message'
+import { deleteMessage, getChatMessages } from '../controllers/message'
 import { authMiddleware } from '../middleware/auth'
 const router = express.Router()
 
 router.route('/chat/:id').get(authMiddleware, getChatMessages)
+router.route('/delete').delete(authMiddleware,deleteMessage)
 export default router
