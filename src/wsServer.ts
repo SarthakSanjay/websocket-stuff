fbfa85b4af657ccf93a457dfb15cb174e63459f2
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-24 23:54:36 +0530

    add: custom aws s3 object url

--- src/wsServer.ts
+++ src/wsServer.ts
@@ -2,7 +2,6 @@ import { WebSocketServer } from "ws";
 import http from "http";
 import url from "url";
 import { PrismaClient } from "@prisma/client";
-import { getObjectUrl, putObject } from "./S3_bucket/aws";
 
 const prisma = new PrismaClient();
 
@@ -14,17 +13,13 @@ interface CustomWebSocket extends WebSocket {
 }
 wss.on("connection", (connection: CustomWebSocket, req) => {
   let userId = decodeURIComponent(url.parse(req.url, true).pathname.slice(1));
-  // console.log(userId);
+
   connection.userId = parseInt(userId);
   // @ts-ignore
   connection.on("message", async (message) => {
     const { friendId, data, messageType, filename, contentType } = JSON.parse(
       message.toString()
     );
-      console.log(friendId,data,messageType,filename,contentType);
-    // console.log('receivedMessage',{
-    //     friendId,data,messageType,filename,contentType
-    // });
 
     const friends = await prisma.friendship.findFirst({
       where: {
@@ -39,44 +34,40 @@ wss.on("connection", (connection: CustomWebSocket, req) => {
       return;
     }
 
-    let url = await getObjectUrl(`images/${filename}.${contentType.split('/')[1]}`)
-    // console.log('url',url);
-    if(url && messageType !== 'TEXT'){
-        await prisma.message.create({
-          data: {
-            senderId: parseInt(userId),
-            receiverId: friendId,
-            data: url,
-            messageType: messageType,
-          },
-        });
+    if (messageType !== "TEXT") {
+      let url = `https://sharko-bucket.s3.ap-south-1.amazonaws.com/${
+        contentType.split("/")[0]
+      }s/${filename}.${contentType.split("/")[1]}`;
 
+      await prisma.message.create({
+        data: {
+          senderId: parseInt(userId),
+          receiverId: friendId,
+          data: url,
+          messageType: messageType,
+        },
+      });
     }
 
-    if(messageType === 'TEXT'){
-        await prisma.message.create({
-            data: {
-              senderId: parseInt(userId),
-              receiverId: friendId,
-              data: data,
-              messageType: messageType,
-            },
-          });
+    if (messageType === "TEXT") {
+      await prisma.message.create({
+        data: {
+          senderId: parseInt(userId),
+          receiverId: friendId,
+          data: data,
+          messageType: messageType,
+        },
+      });
     }
-
-    
 
     wss.clients.forEach(async (client: any) => {
       if (client.userId === friendId) {
-        // console.log('sentMessage',{
-        //     friendId,data,messageType,filename,contentType
-        // });
         client.send(
           JSON.stringify({
             friendId: friendId,
             data: data,
             messageType: messageType,
-            contentType : contentType,
+            contentType: contentType,
             filename: filename,
             createdAt: new Date(),
           })
